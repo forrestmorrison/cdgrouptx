22df6cea83dc7e0cae0c235aa51528cc3b723e1a
forrestmorrison <forrestmorrison@gmail.com>
2023-09-11 12:38:53 -0500

    added content to services page

--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -2,7 +2,36 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <div className="services">
+      <h1>Some of our Initiatives</h1>
+      <section>
+        <h3>Launching Your Business</h3>
+        <div className="service-items">
+          <p>• Personal Evaluation</p>
+          <p>• Strategic Game Planning</p>
+          <p>• Shadowing</p>
+          <p>• Work and Listen</p>
+        </div>
+      </section>
+      <section>
+        <h3>Business Coaching</h3>
+        <div className="service-items">
+          <p>• Continuing Education</p>
+          <p>• Training Conferences</p>
+          <p>• Small Group Dynamics</p>
+          <p>• One-on-One Mentorship</p>
+        </div>
+      </section>
+      <section>
+        <h3>Personal & Leadership Development</h3>
+        <div className="service-items">
+          <p>• Budgeting</p>
+          <p>• Cash Flow Management</p>
+          <p>• Evaluation / Personality Assessment</p>
+          <p>• Coaching</p>
+        </div>
+      </section>
+    </div>
   )
 }
 
