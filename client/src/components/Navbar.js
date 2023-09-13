8dcbfa7868bbdb77b595afd8fa387135d63d6436
forrestmorrison <forrestmorrison@gmail.com>
2023-09-13 16:54:48 -0500

    added mobile menu

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,43 +1,82 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import { FaBars, FaTimes } from "react-icons/fa" 
 import Logo from "../images/cdgroup-logo.png"
 
 const Navbar = () => {
-  return (
-    <div className="navbar">
-        <Link to="/"
-            style={{
-                height: "100%",
-                display: "flex",
-                alignItems: "center"
-            }}
-        >
-            <img src={Logo} alt="logo" className="logo" />
-        </Link>
-        <div className="links">
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
+
+    return (
+        <>
+        <div className="navbar">
             <Link to="/"
                 style={{
-                    margin: "0 20px",
+                    height: "100%",
+                    display: "flex",
+                    alignItems: "center"
                 }}
             >
-                <a href='/'>home</a>
+                <img src={Logo} alt="logo" className="logo" />
             </Link>
-            <Link to="/our-leadership"
-                style={{
-                    margin: "0 20px",
-                }}
-            >
-                <a href='/our-leadership'>our leadership</a>
-            </Link>
-            <Link to="/services"
-                style={{
-                    margin: "0 20px",
-                }}
-            >
-                <a href='/services'>services</a>
-            </Link>
+            
+            {/* Menu */}
+
+            <div className="links">
+                <Link to="/"
+                    style={{
+                        margin: "0 20px",
+                    }}
+                >
+                    <a href='/'>home</a>
+                </Link>
+                <Link to="/our-leadership"
+                    style={{
+                        margin: "0 20px",
+                    }}
+                >
+                    <a href='/our-leadership'>our leadership</a>
+                </Link>
+                <Link to="/services"
+                    style={{
+                        margin: "0 20px",
+                    }}
+                >
+                    <a href='/services'>services</a>
+                </Link>
+            </div>
+
+            {/* Hamburger */}
+
+            <div onClick={handleClick} className="hamburger">
+                { !nav ? <FaBars /> : <FaTimes /> }
+            </div>
         </div>
-    </div>
-  )
+        <div className="mobile-links">
+                <Link to="/"
+                    style={{
+                        margin: "0 20px",
+                    }}
+                >
+                    <a href='/'>home</a>
+                </Link>
+                <Link to="/our-leadership"
+                    style={{
+                        margin: "0 20px",
+                    }}
+                >
+                    <a href='/our-leadership'>our leadership</a>
+                </Link>
+                <Link to="/services"
+                    style={{
+                        margin: "0 20px",
+                    }}
+                >
+                    <a href='/services'>services</a>
+                </Link>
+            </div>
+        </>
+    )
 }
 
 export default Navbar
