3445aa37b19d72c9766442faf95e853a935b9bdd
forrestmorrison <forrestmorrison@gmail.com>
2023-10-01 20:52:45 -0500

    removed index.js

--- server/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const request = require("request")
-
-const app = express()
-
-const PORT = process.env.PORT || 5000
-
-app.listen(PORT, () => { console.log("Server is listening on port 5000") })
